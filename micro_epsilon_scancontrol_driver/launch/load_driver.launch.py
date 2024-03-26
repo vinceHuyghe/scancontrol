00d2157ab1b3a316b86feadc9b4ed79cdaa1db5b
vinceHuyghe <vinceHuyghe@users.noreply.github.com>
2024-03-26 15:07:20 +0000

    fix node

--- micro_epsilon_scancontrol_driver/launch/load_driver.launch.py
+++ micro_epsilon_scancontrol_driver/launch/load_driver.launch.py
@@ -1,22 +1,21 @@
-import os
-import sys
-
 import launch
 import launch_ros.actions
 
 
 def generate_launch_description():
-    ld = launch.LaunchDescription([
-        launch_ros.actions.Node(
-            package='micro_epsilon_scancontrol_driver',
-            executable='driver_node',
-            name='scancontrol_driver',
-            output='screen',
-            on_exit=launch.actions.Shutdown()
-        )
-    ])
+    ld = launch.LaunchDescription(
+        [
+            launch_ros.actions.Node(
+                package="micro_epsilon_scancontrol_driver",
+                executable="driver_node",
+                name="scancontrol_driver",
+                output="screen",
+                on_exit=launch.actions.Shutdown(),
+            )
+        ]
+    )
     return ld
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     generate_launch_description()
--- micro_epsilon_scancontrol_driver/src/node.cpp
+++ micro_epsilon_scancontrol_driver/src/node.cpp
@@ -6,13 +6,15 @@ static const rclcpp::Logger logger = rclcpp::get_logger("scancontrol_driver");
 int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
-    rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("scancontrol_driver"); 
-    rclcpp::Node::SharedPtr private_node = rclcpp::Node::make_shared("scancontrol_driver"); 
+    // rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("scancontrol_driver"); 
+    // rclcpp::Node::SharedPtr private_node = rclcpp::Node::make_shared("scancontrol_driver"); 
+    auto node = std::make_shared<rclcpp::Node>("scancontrol_driver");
 
     // Start the driver
     try
     {
-        scancontrol_driver::ScanControlDriver driver(node, private_node);
+        // scancontrol_driver::ScanControlDriver driver(node, private_node);
+        scancontrol_driver::ScanControlDriver driver(node, node);
         RCLCPP_INFO(logger, "Driver started");
 
         // Loop driver until shutdown
